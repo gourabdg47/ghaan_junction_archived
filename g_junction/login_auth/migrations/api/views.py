464eb479f3760ceb79244f0327340a42e9edf142
gourabdg47 <gourabdasgupta26994@gmail.com>
2024-02-18 22:43:17 +0530

    login_auth api base

---
+++ g_junction/login_auth/migrations/api/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+
+    return JsonResponse(routes, safe=False)
